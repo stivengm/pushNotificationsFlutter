100b1ad9486f1640e6649d87245b919d8a716ad8
Stiven Gomez <stivengomez@soyyo.co>
2023-04-25 13:41:55 -0500

    ADD - Add notifications bloc and config.

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:push_notifications/config/router/app_router.dart';
+
 import 'package:push_notifications/config/theme/app_theme.dart';
+import 'package:push_notifications/presentation/blocs/notifications/notifications_bloc.dart';
 
 void main() {
-  runApp(const MyApp());
+  MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (context) => NotificationsBloc(),
+      ),
+    ],
+    child: const MyApp(),
+  );
 }
 
 class MyApp extends StatelessWidget {
